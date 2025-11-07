9dfea34473e1a50b23d5e52f3dbeacd1dea8959f
jhana_molina <jhanavi60@gmail.com>
2025-11-07 12:44:31 -0500

    Cambios Actualizados

--- PruebaGIT/src/App.java
+++ PruebaGIT/src/App.java
@@ -1,5 +1,8 @@
+import g3_Poliretos.Controller;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Controller controller = new Controller();
+        controller.inicializar();
     }
 }
