4f61e34d1f24dac7367f5b032bca2f61c71ee561
mat264 <lopezjohn2642@gmail.com>
2025-11-09 11:21:43 -0500

    Revert "Carpetas para array y loading. Editó controller"
    This reverts commit 6897d7b7828bece44e3d5f892dfba5392785d867.

--- g3_Proyecto_Poliretos/src/g3_Poliretos/Controller.java
+++ g3_Proyecto_Poliretos/src/g3_Poliretos/Controller.java
@@ -2,39 +2,9 @@ package g3_Poliretos;
 
 import java.util.Scanner;
 
-import g3_Array.g3_Arrays1;
-import g3_Array.g3_Arrays2;
-import g3_Array.g3_Arrays3;
-import g3_Array.g3_Arrays4;
-import g3_Array.g3_Arrays5;
-import g3_Loading.g3_Loading1;
-import g3_Loading.g3_Loading10;
-import g3_Loading.g3_Loading11;
-import g3_Loading.g3_Loading12;
-import g3_Loading.g3_Loading2;
-import g3_Loading.g3_Loading3;
-import g3_Loading.g3_Loading4;
-import g3_Loading.g3_Loading5;
-import g3_Loading.g3_Loading6;
-import g3_Loading.g3_Loading7;
-import g3_Loading.g3_Loading8;
-import g3_Loading.g3_Loading9;
-import g3_LoadingPoliretos.g3_CadenaDeCaracteres.*;
-import g3_LoadingPoliretos.g3_SerieCaracteres.*;
-import g3_LoadingPoliretos.g3_SerieNumerica.*;
-import g3_SerieCaracteres.g3_SerieCaracter1;
-import g3_SerieCaracteres.g3_SerieCaracter2;
-import g3_SerieNumerica.g3_SerieNum1;
-import g3_SerieNumerica.g3_SerieNum10;
-import g3_SerieNumerica.g3_SerieNum12;
-import g3_SerieNumerica.g3_SerieNum2;
-import g3_SerieNumerica.g3_SerieNum3;
-import g3_SerieNumerica.g3_SerieNum4;
-import g3_SerieNumerica.g3_SerieNum5;
-import g3_SerieNumerica.g3_SerieNum6;
-import g3_SerieNumerica.g3_SerieNum7;
-import g3_SerieNumerica.g3_SerieNum8;
-import g3_SerieNumerica.g3_SerieNum9;
+import g3_Poliretos.g3_SerieCaracteres.*;
+import g3_Poliretos.g3_SerieNumerica.*;
+import g3_Poliretos.g3_CadenaDeCaracteres.*;
 
 public class Controller {
     public void inicializar () {
@@ -51,6 +21,7 @@ public class Controller {
 
        System.out.print("Ingrese el numero de terminos a generar: ");
        int n = ingresoDatos.nextInt();
+       ingresoDatos.nextLine(); 
 
        while(n <= 0){
         System.out.println("El número que ingreso es incorrecto");
@@ -126,11 +97,8 @@ public class Controller {
         serieCaracter2.S2Caracter_for(n);
         serieCaracter2.S2Caracter_while(n);
         serieCaracter2.S2Caracter_do_while(n);
-    
 
-    System.out.println("--------- EJERCICIOS ARRAYS -----------");
 
-<<<<<<< Updated upstream
         g3_SerieCaracter3 serieCaracter3 = new g3_SerieCaracter3();
         serieCaracter3.S3Caracter_for(n);
         serieCaracter3.S3Caracter_while(n);
@@ -173,81 +141,36 @@ public class Controller {
 
 
         System.out.println("----- CADENA DE CARACTERES -----");
-=======
-        g3_Arrays1 array1 = new g3_Arrays1();
-        array1.g3_arrayA01_for();
-        array1.g3_arrayA01_while();
-        array1.g3_arrayA01_do_while();
->>>>>>> Stashed changes
 
-        g3_Arrays2 array2 = new g3_Arrays2();
-        array2.g3_arrayA02_for(n); 
-        array2.g3_arrayA02_while(n);
-        array2.g3_arrayA02_do_while(n);
+        g3_CadenaDeCaracteres1 cadena1 = new g3_CadenaDeCaracteres1();
+        cadena1.Cadena1();
+        
+        g3_CadenaDeCaracteres2 cadena2 = new g3_CadenaDeCaracteres2();
+        cadena2.Cadena2();
+        
+        g3_CadenaDeCaracteres3 cadena3 = new g3_CadenaDeCaracteres3();
+        cadena3.Cadena3(ingresoDatos);  
 
-        g3_Arrays3 array3 = new g3_Arrays3();
-        array3.g3_arrayA03_for(n); 
-        array3.g3_arrayA03_while(n);
-        array3.g3_arrayA03_do_while(n);
+        g3_CadenaDeCaracteres4 cadena4 = new g3_CadenaDeCaracteres4();
+        cadena4.Cadena4(ingresoDatos);
 
-        g3_Arrays4 array4 = new g3_Arrays4();
-        array4.g3_arrayA04_for(); 
-        array4.g3_arrayA04_while();
-        array4.g3_arrayA04_do_while();
+        g3_CadenaDeCaracteres5 cadena5 = new g3_CadenaDeCaracteres5();
+        cadena5.Cadena5(ingresoDatos);
 
-        g3_Arrays5 array5 = new g3_Arrays5();
-        array5.g3_arrayA05_for(); 
-        array5.g3_arrayA05_while();
-        array5.g3_arrayA05_do_while();
-    
-    
-    System.out.println("--------- EJERCICIOS LOADING L01-L12 -----------");
+        g3_CadenaDeCaracteres6 cadena6 = new g3_CadenaDeCaracteres6();
+        cadena6.Cadena6(ingresoDatos);
 
-        g3_Loading1 l01 = new g3_Loading1();
-        l01.g3_L01_for(); l01.g3_L01_while(); l01.g3_L01_do_while();
+        g3_CadenaDeCaracteres7 cadena7 = new g3_CadenaDeCaracteres7();
+        cadena7.Cadena7(ingresoDatos);
         
-        g3_Loading2 l02 = new g3_Loading2();
-        l02.g3_L02_for(); l02.g3_L02_while(); l02.g3_L02_do_while();
-        
-        g3_Loading3 l03 = new g3_Loading3();
-        l03.g3_L03_for(); l03.g3_L03_while(); l03.g3_L03_do_while();
-        
-        g3_Loading4 l04 = new g3_Loading4();
-        l04.g3_L04_for(); l04.g3_L04_while(); l04.g3_L04_do_while();
-        
-        g3_Loading5 l05 = new g3_Loading5();
-        l05.g3_L05_for(); l05.g3_L05_while(); l05.g3_L05_do_while();
-        
-        g3_Loading6 l06 = new g3_Loading6();
-        l06.g3_L06_for(); l06.g3_L06_while(); l06.g3_L06_do_while();
-        
-        g3_Loading7 l07 = new g3_Loading7();
-        l07.g3_L07_for(); l07.g3_L07_while(); l07.g3_L07_do_while();
-        
-        g3_Loading8 l08 = new g3_Loading8();
-        l08.g3_L08_for(); l08.g3_L08_while(); l08.g3_L08_do_while();
-        
-        g3_Loading9 l09 = new g3_Loading9();
-        l09.g3_L09_for(); l09.g3_L09_while(); l09.g3_L09_do_while();
-        
-        g3_Loading10 l10 = new g3_Loading10();
-        l10.g3_L10_for(); l10.g3_L10_while(); l10.g3_L10_do_while();
-        
-        g3_Loading11 l11 = new g3_Loading11();
-        
-        System.out.println("--- L11a (Simétrico) ---");
+        g3_CadenaDeCaracteres8 cadena8 = new g3_CadenaDeCaracteres8();
+        cadena8.Cadena8(ingresoDatos);
 
-        l11.g3_L11a_for(n); l11.g3_L11a_while(n); l11.g3_L11a_do_while(n);
+        g3_CadenaDeCaracteres9 cadena9 = new g3_CadenaDeCaracteres9();
+        cadena9.Cadena9(ingresoDatos);
 
-        System.out.println("--- L11b (Vectorial) ---");
-        l11.g3_L11b_for(n); l11.g3_L11b_while(n); l11.g3_L11b_do_while(n);
-        
-        g3_Loading12 l12 = new g3_Loading12();
-        l12.g3_L12_for(n); l12.g3_L12_while(n); l12.g3_L12_do_while(n);
-    
-    
-    
-    
-    
+
+
+        ingresoDatos.close(); 
     }
-} 
+}
